873988d23cb1e8503f876eac6b0b815c362e083f
John Lee <john.lee@sv.cmu.edu>
2013-07-14 17:12:10 -0700

    add capture photo functionality

--- www/js/killculator.js
+++ www/js/killculator.js
@@ -13,6 +13,10 @@ var dbDesc = "Shotgun Database 1.0"
 var dbSize = 5 * 1024 * 1024; 
 var db;
 
+//camera variables
+var pictureSource;   // picture source
+var destinationType; // sets the format of returned value 
+
 // Populate the database 
 function populateDB(tx) {
     tx.executeSql('DROP TABLE IF EXISTS SHOTS');
@@ -55,6 +59,10 @@ function successCB() {
 
 // Cordova is ready
 function onDeviceReady() {
+    //set default camera variables
+    pictureSource=navigator.camera.PictureSourceType;
+    destinationType=navigator.camera.DestinationType;
+
     if (window.openDatabase) {
       var db = window.openDatabase(dbName, dbVersion, dbDesc, dbSize);
       db.transaction(populateDB, errorCB, successCB);
@@ -96,6 +104,71 @@ function showAlert(message, title, buttonName) {
 function alertDismissed() {
     // do something
 }
+
+
+//** START CAMERA JS
+// Called when a photo is successfully retrieved
+function onPhotoDataSuccess(imageData) {
+  // Uncomment to view the base64 encoded image data
+  //console.log(imageData);
+
+  // Get image handle
+  var smallImage = document.getElementById('targetImage');
+
+  // Unhide image elements
+  smallImage.style.display = 'block';
+
+  // Show the captured photo
+  // The inline CSS rules are used to resize the image
+  smallImage.src = "data:image/jpeg;base64," + imageData;
+}
+
+// Called when a photo is successfully retrieved
+function onPhotoURISuccess(imageURI) {
+  // Uncomment to view the image file URI 
+  console.log(imageURI);
+  // Get image handle
+  //
+  var largeImage = document.getElementById('targetImage');
+
+  // Unhide image elements
+  largeImage.style.display = 'block';
+
+  // Show the captured photo
+  // The inline CSS rules are used to resize the image
+  largeImage.src = imageURI;
+}
+
+// A button will call this function
+function capturePhoto() {
+  // Take picture using device camera and retrieve image as base64-encoded string
+  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+    destinationType: destinationType.DATA_URL });
+}
+
+// A button will call this function
+function capturePhotoEdit() {
+  // Take picture using device camera, allow edit, and retrieve image as base64-encoded string  
+  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
+    destinationType: destinationType.DATA_URL });
+}
+
+// A button will call this function
+//
+function getPhoto(source) {
+  // Retrieve image file location from specified source
+  navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, 
+    destinationType: destinationType.FILE_URI,
+    sourceType: source });
+}
+
+// Called if something bad happens.
+// 
+function onFail(message) {
+  alert('Failed because: ' + message);
+}
+
+//** END CAMERA JS    
 
 
 
